979478f32fc7fb7ef2e9fe53ed33c489ae8a1e78
MarcusR91 <Marcus_R91@hotmail.com>
2021-09-13 14:39:41 +0200

    Lade till funktioner och en localstorage

--- script.js
+++ script.js
@@ -3,21 +3,43 @@ const password = "1234";
 const btn = document.getElementById("btn");
 
 const inpValue = document.getElementById("user-name");
-const pswValue =document.getElementById("password");
+const pswValue = document.getElementById("password");
 
+const newDiv = document.createElement("div")
+document.body.appendChild(newDiv);
 
 btn.addEventListener('click', function(){
 
+    
+    localStorage.setItem("Name", inpValue.value);
+   
+
     if(inpValue.value === userName && pswValue.value === password ){
         welcome();
+        
     }
     else{
-        alert("Fel")
+        notValid();
     }
 })
 
 function welcome(){
     document.getElementById("user-name").style.visibility = "hidden";
     document.getElementById("password").style.visibility = "hidden";
-
+    document.getElementById("btn").style.visibility = "hidden";
+    
+    const h1 = document.createElement('h1');
+    newDiv.appendChild(h1);
+    h1.innerText = "Välkommen";
 }
+function notValid(){
+    document.getElementById("user-name").style.visibility = "hidden";
+    document.getElementById("password").style.visibility = "hidden";
+    document.getElementById("btn").style.visibility = "hidden";
+
+    const h1 = document.createElement('h1');
+    newDiv.appendChild(h1);
+    h1.innerText = "Fel Lösenord";
+}
+
+
