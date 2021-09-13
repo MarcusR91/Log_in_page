891cf3af17ef07eda92c5138a751f385e47e5b30
MarcusR91 <Marcus_R91@hotmail.com>
2021-09-13 16:28:03 +0200

    Lade till funtioner som sparar min storage

--- script.js
+++ script.js
@@ -10,28 +10,30 @@ document.body.appendChild(newDiv);
 
 btn.addEventListener('click', function(){
 
-    
-    localStorage.setItem("Name", inpValue.value);
    
 
     if(inpValue.value === userName && pswValue.value === password ){
+        localStorage.setItem("Name", inpValue.value);
         welcome();
-        
+        loggedIn();  
     }
     else{
         notValid();
     }
 })
 
+// Välkomstsida för den inloggade användaren
 function welcome(){
     document.getElementById("user-name").style.visibility = "hidden";
     document.getElementById("password").style.visibility = "hidden";
     document.getElementById("btn").style.visibility = "hidden";
     
-    const h1 = document.createElement('h1');
-    newDiv.appendChild(h1);
-    h1.innerText = "Välkommen";
+    // const h1 = document.createElement('h1');
+    // newDiv.appendChild(h1);
+    // h1.innerText = "Välkommen";
 }
+
+// Fel meddelande om inlogg uppgifterna är fel
 function notValid(){
     document.getElementById("user-name").style.visibility = "hidden";
     document.getElementById("password").style.visibility = "hidden";
@@ -41,5 +43,23 @@ function notValid(){
     newDiv.appendChild(h1);
     h1.innerText = "Fel Lösenord";
 }
+function loggedIn(){
+   let store = localStorage.getItem("Name");
+   newDiv.innerHTML = "välommen " + `${store}`;
 
+   const eraseButton = document.createElement("button"); // skapar erasebutton
+   eraseButton.innerHTML = "Logout";
+   newDiv.appendChild(eraseButton);
+   
+   eraseButton.addEventListener('click', function(){ 
+       localStorage.removeItem("Name"); // tar bort item från localstorage
+       newDiv.innerHTML = ""; // Div vlir en tome textsträng
+
+       document.getElementById("user-name").style.visibility = "visible";
+       document.getElementById("password").style.visibility = "visible";
+       document.getElementById("btn").style.visibility = "visible";
+
+   })
+
+}
 
